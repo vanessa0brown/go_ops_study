52f864f89d24a860bd06791c665e03085edbc0dd
Aleks <no@mail.com>
2024-09-16 15:36:10 +0300

    start 5_2 Linked_List

---
+++ 5_2_Linked_list/go.mod
@@ -0,0 +1,3 @@
+module 5_2_Linked_list
+
+go 1.23
---
+++ 5_2_Linked_list/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func CreateList(sl []int) *ListNode {
+	res := ListNode{}
+	cur := &res
+	for _, val := range sl {
+		if cur.Next != nil {
+			cur = cur.Next
+		}
+		cur.Val = val
+		cur.Next = &ListNode{}
+	}
+	if cur.Next.Val == 0 {
+		cur.Next = nil
+	}
+	return &res
+}
+
+func deleteDuplicates(l *ListNode) {
+	//map
+}
+
+func main() {
+	list := CreateList([]int{1, 2, 3})
+	fmt.Println(list.Val)
+	fmt.Println(list.Next.Val)
+	fmt.Println(list.Next.Next.Val)
+}
